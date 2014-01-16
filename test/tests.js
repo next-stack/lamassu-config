0c797b81645b8696b6e2b7c4ac1fb56c1476994c
Alan Bradley <alan@gangleri.net>
2014-01-16 22:45:36 +0000

    Tests for reading exchanges config

--- test/tests.js
+++ test/tests.js
@@ -14,3 +14,16 @@ test('Load config', function(t){
     t.end();
   });
 });
+
+
+test('Load Exchanges config', function(t){
+  config.readExchangesConfig(function(err, config) {
+    t.equal(err, null, 'There should be no error.')
+    t.notEqual(config, null, 'Config should  have been read.');
+    t.notEqual(config.exchanges, null, 'Exchanges config should be included.');
+    t.notEqual(config.exchanges.settings, null, 'Exchanges should have settings.');
+    t.notEqual(config.exchanges.plugins, null, 'Exchanges should have plugins.');
+    t.notEqual(config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
+    t.end();
+  });
+});
