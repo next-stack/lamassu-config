534d067386fb7aca464f1f3b84bd2f212c4a660f
Alan Bradley <alan@gangleri.net>
2014-01-17 11:41:33 +0000

    Updated to wrap config in result object that includes flag to indicate if all worked.

--- test/tests.js
+++ test/tests.js
@@ -4,26 +4,28 @@ var test = require('tap').test
 var config = require('../lib/main');
 
 test('Load config', function(t){
-  config.load(function(err, config) {
-    t.equal(err, null, 'There should be no error.')
-    t.notEqual(config, null, 'Config should  have been read.');
-    t.notEqual(config.exchanges, null, 'Exchanges config should be included.');
-    t.notEqual(config.exchanges.settings, null, 'Exchanges should have settings.');
-    t.notEqual(config.exchanges.plugins, null, 'Exchanges should have plugins.');
-    t.notEqual(config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
+  config.load(function(err, result) {
+    t.equal(err, null, 'There should be no error.');
+    t.ok(result.ok, 'Result should be ok.');
+    t.notEqual(result.config, null, 'Config should  have been read.');
+    t.notEqual(result.config.exchanges, null, 'Exchanges config should be included.');
+    t.notEqual(result.config.exchanges.settings, null, 'Exchanges should have settings.');
+    t.notEqual(result.config.exchanges.plugins, null, 'Exchanges should have plugins.');
+    t.notEqual(result.config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
     t.end();
   });
 });
 
 
 test('Load Exchanges config', function(t){
-  config.readExchangesConfig(function(err, config) {
-    t.equal(err, null, 'There should be no error.')
-    t.notEqual(config, null, 'Config should  have been read.');
-    t.notEqual(config.exchanges, null, 'Exchanges config should be included.');
-    t.notEqual(config.exchanges.settings, null, 'Exchanges should have settings.');
-    t.notEqual(config.exchanges.plugins, null, 'Exchanges should have plugins.');
-    t.notEqual(config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
+  config.readExchangesConfig(function(err, result) {
+    t.equal(err, null, 'There should be no error.');
+    t.ok(result.ok, 'Result should be ok.');
+    t.notEqual(result.config, null, 'Config should  have been read.');
+    t.notEqual(result.config.exchanges, null, 'Exchanges config should be included.');
+    t.notEqual(result.config.exchanges.settings, null, 'Exchanges should have settings.');
+    t.notEqual(result.config.exchanges.plugins, null, 'Exchanges should have plugins.');
+    t.notEqual(result.config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
     t.end();
   });
 });
