d838500b261a6d44abaa68e6c1ff2b8933e66be5
Maciej Małecki <me@mmalecki.com>
2014-02-28 23:53:32 +0100

    Make `lamassu-config` a class
    Remove enormous amount of state, cached connection string, make API
    nicer.

--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,12 @@
 'use strict';
 
 var test = require('tap').test
-var config = require('../lib/main');
+var LamassuConfig = require('../lib/main');
+var con = 'psql://lamassu:lamassu@localhost/lamassu';
 
 test('Load config', function(t){
+  var config = new LamassuConfig(con);
   config.load(function(err, result) {
-    t.equal(err, null, 'There should be no error.');
-    t.ok(result.ok, 'Result should be ok.');
-    t.notEqual(result.config, null, 'Config should  have been read.');
-    t.notEqual(result.config.exchanges, null, 'Exchanges config should be included.');
-    t.notEqual(result.config.exchanges.settings, null, 'Exchanges should have settings.');
-    t.notEqual(result.config.exchanges.plugins, null, 'Exchanges should have plugins.');
-    t.notEqual(result.config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
-    t.end();
-  });
-});
-
-
-test('Load Exchanges config', function(t){
-  config.readExchangesConfig(function(err, result) {
     t.equal(err, null, 'There should be no error.');
     t.ok(result.ok, 'Result should be ok.');
     t.notEqual(result.config, null, 'Config should  have been read.');
