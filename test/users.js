3472fc9e22152ea1875738929ba99b9c1a20d145
Joaquim Serafim <joaquim.serafim@gmail.com>
2014-03-22 16:11:09 +0000

    Add test for username isn't in the table, removed extra dependency

--- test/users.js
+++ test/users.js
@@ -5,8 +5,8 @@ var LamassuConfig = require('../lib/main');
 var con = 'psql://lamassu:lamassu@localhost/lamassu';
 
 
-test('Load config', function(t) {
-  t.plan(3);
+test('Auth user - create an user & auth, next try auth an user isn\'t exist', function(t) {
+  t.plan(4);
 
   var config = new LamassuConfig(con);
 
@@ -18,7 +18,11 @@ test('Load config', function(t) {
 
     config.authenticateUser(user, pwd, function (_err, auth) {
       t.equal(_err, null, 'There should be no error.');
-      t.equal(auth, true, 'There should only accept an authenticate user and return true');
+      t.equal(auth, true, 'There should only accept an authenticate user.');
+
+      config.authenticateUser('bad_user', 'bad_pwd', function (err) {
+        t.ok(err, 'There should auth an user isn\'t exist & return the ERROR msg: ' + err);
+      });
     });
   });
 
