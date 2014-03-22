1e76aa6eddb021ba3ccce60fa74a759fce64cd4b
Joaquim Serafim <joaquim.serafim@gmail.com>
2014-03-22 15:23:38 +0000

    Change the test messages

--- test/users.js
+++ test/users.js
@@ -14,11 +14,11 @@ test('Load config', function(t) {
   var pwd = 'test';
 
   config.saveUser(user, pwd, function (err) {
-    t.equal(err, null, 'There should be INSERT op.');
+    t.equal(err, null, 'There should create a new user and return no error');
 
     config.authenticateUser(user, pwd, function (_err, auth) {
       t.equal(_err, null, 'There should be no error.');
-      t.equal(auth, true, 'The SELECT op should return true');
+      t.equal(auth, true, 'There should only accept an authenticate user and return true');
     });
   });
 
