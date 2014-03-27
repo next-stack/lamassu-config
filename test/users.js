6e812c8625f82598b37de0d3c44ee4a506ead141
Maciej Małecki <me@mmalecki.com>
2014-03-27 07:48:08 +0700

    Add `LamassuConfig#updatePassword`
    Rename `saveUser` to `addUser`.
    Refactor our a `hash` function.

--- test/users.js
+++ test/users.js
@@ -6,19 +6,29 @@ var con = 'psql://lamassu:lamassu@localhost/lamassu';
 
 
 test('Auth user - create an user & auth, next try auth an user isn\'t exist', function(t) {
-  t.plan(5);
+  t.plan(8);
 
   var config = new LamassuConfig(con);
 
   var user = 'test';
   var pwd = 'test';
+  var newPwd = 'hunter12';
 
-  config.saveUser(user, pwd, function (err) {
+  config.addUser(user, pwd, function (err) {
     t.equal(err, null, 'There should create a new user and return no error.');
 
     config.authenticateUser(user, pwd, function (err, auth) {
       t.equal(err, null, 'There should be no error.');
       t.equal(auth, true, 'There should only accept an authenticate user.');
+
+      config.updatePassword(user, newPwd, function(err) {
+        t.equal(err, null, 'There should be no error when updating password.');
+
+        config.authenticateUser(user, newPwd, function (err, auth) {
+          t.equal(err, null, 'There should be no error.');
+          t.equal(auth, true, 'Newly changed password should authenticate correctly');
+        });
+      });
     });
   });
 
