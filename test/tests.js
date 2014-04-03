b7cab9d32acef572bb446b8ebe208817dad85beb
Maciej Małecki <me@mmalecki.com>
2014-04-04 05:31:14 +0700

    Remove superfluous success indication
    If we run into an error, we should just return it as the first
    parameter.

--- test/tests.js
+++ test/tests.js
@@ -8,12 +8,11 @@ test('Load config', function(t){
   var config = new LamassuConfig(con);
   config.load(function(err, result) {
     t.equal(err, null, 'There should be no error.');
-    t.ok(result.ok, 'Result should be ok.');
-    t.notEqual(result.config, null, 'Config should  have been read.');
-    t.notEqual(result.config.exchanges, null, 'Exchanges config should be included.');
-    t.notEqual(result.config.exchanges.settings, null, 'Exchanges should have settings.');
-    t.notEqual(result.config.exchanges.plugins, null, 'Exchanges should have plugins.');
-    t.notEqual(result.config.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
+    t.notEqual(result, null, 'Config should  have been read.');
+    t.notEqual(result.exchanges, null, 'Exchanges config should be included.');
+    t.notEqual(result.exchanges.settings, null, 'Exchanges should have settings.');
+    t.notEqual(result.exchanges.plugins, null, 'Exchanges should have plugins.');
+    t.notEqual(result.exchanges.plugins.settings, null, 'Exchanges plugins should have settings.');
     t.end();
 
     config.end();
