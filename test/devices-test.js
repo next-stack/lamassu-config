2996cf116d95388066e58605e6a2e8180d8a193a
Maciej Małecki <me@mmalecki.com>
2014-04-05 15:24:44 +0700

    No need to test authorizing either

--- test/devices-test.js
+++ test/devices-test.js
@@ -9,7 +9,7 @@ var authorized = 'CB:3D:78:49:03:39:BA:47:0A:33:29:3E:31:25:F7:C6:4F:74:71:D7';
 test('authorize and check authorization', function(t){
   var config = new LamassuConfig(con, 1);
 
-  t.plan(16);
+  t.plan(15);
 
   config.isAuthorized('AA:BB:CC:DD:EE:FF:00:11:22:33:44:55:66:77:88:99:4F:74:71:D7', function(err, authorized) {
     t.equal(err, null, 'There should be no error.');
@@ -25,20 +25,16 @@ test('authorize and check authorization', function(t){
     config.pair(token, authorized, 'bar', function(err) {
       t.equal(err, null, 'There should be no error');
 
-      config.authorize(authorized, function(err) {
-        t.equal(err, null, 'There should be no error when authorizing the device');
+      config.isAuthorized(authorized, function(err, isAuthorized) {
+        t.equal(err, null, 'There should be no error when checking authorization');
+        t.equal(isAuthorized, true, 'device just authorized should come up as authorized');
 
-        config.isAuthorized(authorized, function(err, isAuthorized) {
-          t.equal(err, null, 'There should be no error when checking authorization');
-          t.equal(isAuthorized, true, 'device just authorized should come up as authorized');
+        config.deauthorize(authorized, function(err) {
+          t.equal(err, null, 'There should be no error when deauthorizing');
 
-          config.deauthorize(authorized, function(err) {
-            t.equal(err, null, 'There should be no error when deauthorizing');
-
-            config.isAuthorized(authorized, function(err, authorized) {
-              t.equal(err, null, 'There should be no error when checking authorization');
-              t.equal(authorized, false, 'device just deauthorized should come up as deauthorized');
-            });
+          config.isAuthorized(authorized, function(err, authorized) {
+            t.equal(err, null, 'There should be no error when checking authorization');
+            t.equal(authorized, false, 'device just deauthorized should come up as deauthorized');
           });
         });
       });
